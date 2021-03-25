de46c32d77fcf04eacc4003c4b8578d1271706f1
Michael Shkolniy <171shkolnik@gmail.com>
2021-03-25 13:26:08 +0300

    Applied 3_0_fix.patch

---
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -0,0 +1,62 @@
+package ru.javawebinar.topjava.repository.inmemory;
+
+import org.springframework.util.CollectionUtils;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository.USER_ID;
+
+public class InMemoryMealRepository implements MealRepository {
+    private final Map<Integer, Map<Integer, Meal>> usersMeal = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        for (Meal meal : MealsUtil.meals) {
+            save(meal, USER_ID);
+        }
+    }
+
+    @Override
+    public Meal save(Meal meal, int userId) {
+        Map<Integer, Meal> meals = usersMeal.computeIfAbsent(userId, ConcurrentHashMap::new);
+        if (meal.isNew()) {
+            meal.setId(counter.incrementAndGet());
+            meals.put(meal.getId(), meal);
+            return meal;
+        }
+        // handle case: update, but not present in storage
+
+        return meals.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        Map<Integer, Meal> meals = usersMeal.get(userId);
+        return meals != null && meals.remove(id) != null;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        Map<Integer, Meal> meals = usersMeal.get(userId);
+        return meals == null ? null : meals.get(id);
+    }
+
+    @Override
+    public Collection<Meal> getAll(int userId) {
+        Map<Integer, Meal> meals = usersMeal.get(userId);
+        return CollectionUtils.isEmpty(meals) ? Collections.emptyList() :
+                meals.values().stream()
+                        .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                        .collect(Collectors.toList());
+    }
+}
+
